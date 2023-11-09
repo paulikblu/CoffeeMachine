efe0e1ff8515d67e7367eb109fb35f1633b2a0c3
paulikblu <paul.muntean93@gmail.com>
2023-11-09 18:57:40 +0200

    second commit

--- main.py
+++ main.py
@@ -8,63 +8,62 @@ milk:{milk}
 coffee:{coffee}
 money:{money}$""")
 
-def checking_resources(drink_type):
+def checking_resources(drink_type, water, coffee, milk):
     try:
-        if MENU[drink_type]["ingredients"]["water"] > resources["water"]:
+        if MENU[drink_type]["ingredients"]["water"] > water:
             print("Sorry there is not enough water")
-        if MENU[drink_type]["ingredients"]["milk"] > resources["milk"]:
-            print("Sorry there is not enough milk")
-        if MENU[drink_type]["ingredients"]["coffee"] > resources["coffee"]:
+        if MENU[drink_type]["ingredients"]["coffee"] > coffee:
             print("Sorry there is not enough coffee")
+        if drink_type != "espresso" and MENU[drink_type]["ingredients"]["milk"] > milk:
+            print("Sorry there is not enough milk")
         else:
             return True
     except KeyError:
         print("sorry, invalid drink")
 
 def coins():
-    quarter = 0.25
-    dime = 0.10
-    nickle = 0.05
-    penny = 0.01
-    quarters = quarter * float(input("Insert the number of quarters: "))
-    dimes = dime * float(input("Insert the number of dimes: "))
-    nickles = nickle * float(input("Insert the number of nickles: "))
-    pennies = penny * float(input("Insert the number of pennies: "))
-    sum = quarters + dimes + nickles + pennies
-    return sum
+    try:
+        quarter = 0.25
+        dime = 0.10
+        nickle = 0.05
+        penny = 0.01
+        quarters = quarter * float(input("Insert the number of quarters: "))
+        dimes = dime * float(input("Insert the number of dimes: "))
+        nickles = nickle * float(input("Insert the number of nickles: "))
+        pennies = penny * float(input("Insert the number of pennies: "))
+        sum = quarters + dimes + nickles + pennies
+        return sum
+    except ValueError:
+        print("Invalid number")
+        return 0
 
-def transaction(sum, drink_type):
-    if sum < MENU[drink_type]["cost"]:
-        print("Sorry, not enough money")
-    elif sum > MENU[drink_type]["cost"]:
-        change = sum - MENU[drink_type]["cost"]
-        rounded_change = round(change, 2)
-        cost = MENU[drink_type]["cost"]
-        print(f"Here is ${rounded_change} in change")
-        print("processing order")
+
+water = resources["water"]
+milk = resources["milk"]
+coffee = resources["coffee"]
+money = 0
+
+while working:
+    order = input("What would you like? (espresso/latte/cappuccino): ")
+    if order == "off":
+        working = False
+    elif order == "report":
+        report(water, milk, coffee, money)
     else:
-        cost = sum
-        print("processing order")
-    return cost
-
-b = 0
-b += transaction(4.3, "latte")
-print(b)
-
-# while working:
-#     water = resources["water"]
-#     milk = resources["milk"]
-#     coffee = resources["coffee"]
-#     money = 0
-#     order = input("What would you like? (espresso/latte/cappuccino): ")
-#     if order == "off":
-#         working = False
-#     elif order == "report":
-#         report(water, milk, coffee, money)
-#     else:
-#         checking_resources(order)
-#         if checking_resources(order):
-#             coins = sum()
-#             transaction(coins, order)
-#             money += transaction(coins, order)
-
+        if checking_resources(order, water, coffee, milk):
+            sum = coins()
+            if sum < MENU[order]["cost"]:
+                print("Sorry, not enough money")
+            else:
+                change = sum - MENU[order]["cost"]
+                if change > 0:
+                    rounded_change = round(change, 2)
+                    money += MENU[order]["cost"]
+                    print(f"Here is ${rounded_change} in change")
+                else:
+                    money += sum
+                water -= MENU[order]["ingredients"]["water"]
+                coffee -= MENU[order]["ingredients"]["coffee"]
+                if order != "espresso":
+                    milk -= MENU[order]["ingredients"]["milk"]
+                print(f"Here is your {order}. Enjoy!")
